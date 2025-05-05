0f08604a78701864a45ab6d2b8905cd79aea9d80
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-05-05 15:41:01 -0400

    Try disabling tap, glide and scroll

--- keyboards/splitkb/halcyon/kyria/keymaps/jelmansouri_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/jelmansouri_hlc/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_split_3x6_5_hlc(
        NAV_3D,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_PSCR,                                       KC_NO,    KC_7,    KC_8,    KC_9, KC_TRNS, KC_TRNS,
       KC_TRNS,   KC_F5,   KC_F6,   KC_F7,   KC_F8, MS_BTN1,                                       KC_NO,    KC_4,    KC_5,    KC_6,   KC_NO,   KC_NO,
-      LALT_F4,   KC_F1,   KC_F2,   KC_F3,   KC_F4, MS_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_0,    KC_1,    KC_2,    KC_3, KC_TRNS,   KC_NO,
+      LALT_F4,   KC_F1,   KC_F2,   KC_F3,   KC_F4, DSCROLL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_0,    KC_1,    KC_2,    KC_3, KC_TRNS,   KC_NO,
                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
       _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
@@ -120,9 +120,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-// Function to handle layer changes and disable drag scrolling when not in AUTO_MOUSE_DEFAULT_LAYER
+// Function to handle layer changes and disable drag scrolling
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Disable set_scrolling if the current layer is not the AUTO_MOUSE_DEFAULT_LAYER
     set_scrolling = false;
     return state;
 }
