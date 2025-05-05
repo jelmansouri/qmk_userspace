258083b66cf97969eb12b97e4f2942a046aaa65a
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-05-05 15:55:13 -0400

    Remove layer setting function

--- keyboards/splitkb/halcyon/kyria/keymaps/jelmansouri_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/jelmansouri_hlc/keymap.c
@@ -119,9 +119,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-
-// Function to handle layer changes and disable drag scrolling
-layer_state_t layer_state_set_user(layer_state_t state) {
-    set_scrolling = false;
-    return state;
-}
