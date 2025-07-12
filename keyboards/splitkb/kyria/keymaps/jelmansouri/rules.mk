6ea36d96e419967c4fb7cd1356c931bd1e4ffb90
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-07-12 10:46:10 -0300

    Prepare setup for trackpad support

--- keyboards/splitkb/kyria/keymaps/jelmansouri/rules.mk
+++ keyboards/splitkb/kyria/keymaps/jelmansouri/rules.mk
@@ -1,7 +1,16 @@
-# extra sources
-SRC +=  oled.c
-
 # converting to RP2040ce/liatris
 CONVERT_TO=liatris
 
 CAPS_WORD_ENABLE = yes
+
+ifdef CIRQUE_TRACKPAD
+    OLED_ENABLE = no
+	POINTING_DEVICE_ENABLE = yes
+	POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
+	SRC += trackpad.c
+endif
+
+ifdef OLED_DISPLAY
+	# extra sources
+	SRC +=  oled.c
+endif
