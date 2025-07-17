bccf233b998139208bd65b8ea9c818e0fbcf0be8
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-07-17 00:02:11 -0300

    Remove cirque rotation and simplify build

--- keyboards/splitkb/kyria/keymaps/jelmansouri/rules.mk
+++ keyboards/splitkb/kyria/keymaps/jelmansouri/rules.mk
@@ -3,13 +3,9 @@ CONVERT_TO=liatris
 
 CAPS_WORD_ENABLE = yes
 
-# ifdef CIRQUE_TRACKPAD
-	POINTING_DEVICE_ENABLE = yes
-	POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
-	SRC += trackpad.c
-# endif
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+SRC += trackpad.c
 
-# ifdef OLED_DISPLAY
-	# extra sources
-	SRC +=  oled.c
-# endif
+# extra sources
+SRC +=  oled.c
