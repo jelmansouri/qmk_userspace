0cec1920440f80a72943a3c095ea9da3692c64af
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-08-17 16:55:43 -0300

    Remove space from the default flow tap key impl

--- keyboards/splitkb/kyria/keymaps/jelmansouri/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jelmansouri/keymap.c
@@ -84,3 +84,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 // clang-format on
+
+bool is_flow_tap_key(uint16_t keycode) {
+    if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
+        return false; // Disable Flow Tap on hotkeys.
+    }
+    switch (get_tap_keycode(keycode)) {
+        // case KC_SPC:
+        case KC_A ... KC_Z:
+        case KC_DOT:
+        case KC_COMM:
+        case KC_SCLN:
+        case KC_SLSH:
+            return true;
+    }
+    return false;
+}
