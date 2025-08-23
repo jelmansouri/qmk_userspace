e4b6f3b0319f147961d8498d05a9de987b7a5ea7
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-08-22 23:28:38 -0300

    Add set raw cursor function

--- keyboards/splitkb/kyria/keymaps/jelmansouri/oled.c
+++ keyboards/splitkb/kyria/keymaps/jelmansouri/oled.c
@@ -1,255 +1,100 @@
+#include <stdint.h>
 #include QMK_KEYBOARD_H
 #include "keymap.h"
 
 #ifdef OLED_ENABLE
+
+extern uint8_t* oled_cursor;
+extern uint8_t  oled_buffer[];
+extern uint8_t  oled_rotation_width;
+
+void oled_set_cursor_raw(uint8_t col, uint8_t line) {
+    uint16_t index = line * oled_rotation_width + col * 8;
+
+    // Out of bounds?
+    if (index >= OLED_MATRIX_SIZE) {
+        index = 0;
+    }
+
+    oled_cursor = &oled_buffer[index];
+}
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_180;
 }
 
-// static void render_shift(bool pressed) {
-//     static const char PROGMEM shift_icon[]         = {0x08, 0x0c, 0xf2, 0x81, 0x81, 0xf2, 0x0c, 0x08};
-//     static const char PROGMEM shift_icon_pressed[] = {0xf7, 0xf3, 0x05, 0x7e, 0x7e, 0x05, 0xf3, 0xf7};
-//     oled_write_raw_P(pressed ? shift_icon_pressed : shift_icon, 8);
-// }
-//
-// static void render_ctrl(bool pressed) {
-//     static const char PROGMEM ctrl_icon[]         = {0x08, 0x04, 0x02, 0x01, 0x01, 0x02, 0x04, 0x08};
-//     static const char PROGMEM ctrl_icon_pressed[] = {0xf7, 0xfb, 0xfd, 0xfe, 0xfe, 0xfd, 0xfb, 0xf7};
-//     oled_write_raw_P(pressed ? ctrl_icon_pressed : ctrl_icon, 8);
-// }
-//
-// static void render_option(bool pressed) {
-//     static const char PROGMEM option_icon[]         = {0x01, 0x01, 0x03, 0x1c, 0x60, 0x81, 0x81, 0x81};
-//     static const char PROGMEM option_icon_pressed[] = {0xfe, 0xfe, 0xfc, 0xe3, 0x9f, 0x7e, 0x7e, 0x7e};
-//     oled_write_raw_P(pressed ? option_icon_pressed : option_icon, 8);
-// }
-//
-// static void render_cmd(bool pressed) {
-//     static const char PROGMEM cmd_icon[]         = {0x66, 0xa5, 0xff, 0x24, 0x24, 0xff, 0xa5, 0x66};
-//     static const char PROGMEM cmd_icon_pressed[] = {0x99, 0x5a, 0x00, 0xdb, 0xdb, 0x00, 0x5a, 0x99};
-//     oled_write_raw_P(pressed ? cmd_icon_pressed : cmd_icon, 8);
-// }
-
-static void render_apple_logo(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 128, 128, 192, 192, 192, 192, 192, 192, 128, 128, 152, 158, 198, 195, 195, 195, 192, 192, 128, 128, 0, 0, 0, 0, 0, 0, 0, 0, 240, 252, 30, 7, 3, 1, 0, 0, 0, 0, 0, 0, 1, 3, 3, 1, 0, 0, 0, 0, 0, 224, 249, 31, 7, 3, 0, 0, 0, 0, 0, 0, 7, 63, 248, 224, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 15, 252, 240, 96, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 7, 14, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 14, 7, 3, 1, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
+static void render_shift(bool pressed) {
+    static const char PROGMEM shift_icon[]         = {0x08, 0x0c, 0xf2, 0x81, 0x81, 0xf2, 0x0c, 0x08};
+    static const char PROGMEM shift_icon_pressed[] = {0xf7, 0xf3, 0x05, 0x7e, 0x7e, 0x05, 0xf3, 0xf7};
+    oled_write_raw_P(pressed ? shift_icon_pressed : shift_icon, 8);
 }
 
-// static void render_caps_lock(void) {
-//     static const char PROGMEM raw_logo[] = {
-//         0, 0, 0, 0, 0, 224, 224, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 96, 224, 224, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 0, 0, 0, 128, 192, 224, 240, 120, 60, 60, 120, 240, 224, 192, 128, 0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 0, 0, 0, 49, 51, 49, 48, 48, 48, 48, 48, 48, 49, 51, 49, 0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 0, 0, 0, 0, 0,
-//     };
-//     oled_write_raw_P(raw_logo, sizeof(raw_logo));
-// }
-
-static void render_blank(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
+static void render_ctrl(bool pressed) {
+    static const char PROGMEM ctrl_icon[]         = {0x08, 0x04, 0x02, 0x01, 0x01, 0x02, 0x04, 0x08};
+    static const char PROGMEM ctrl_icon_pressed[] = {0xf7, 0xfb, 0xfd, 0xfe, 0xfe, 0xfd, 0xfb, 0xf7};
+    oled_write_raw_P(pressed ? ctrl_icon_pressed : ctrl_icon, 8);
 }
 
-static void render_mod_ctrl(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 128, 192, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 7, 3, 1, 0, 0, 1, 3, 7, 14, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
+static void render_option(bool pressed) {
+    static const char PROGMEM option_icon[]         = {0x01, 0x01, 0x03, 0x1c, 0x60, 0x81, 0x81, 0x81};
+    static const char PROGMEM option_icon_pressed[] = {0xfe, 0xfe, 0xfc, 0xe3, 0x9f, 0x7e, 0x7e, 0x7e};
+    oled_write_raw_P(pressed ? option_icon_pressed : option_icon, 8);
 }
 
-static void render_mod_shft(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 192, 224, 240, 184, 28, 14, 6, 6, 14, 28, 184, 240, 224, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 31, 31, 48, 48, 48, 48, 31, 31, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_alt(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 28, 126, 220, 152, 0, 0, 152, 252, 254, 28, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 127, 127, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_gui(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 192, 224, 224, 216, 188, 126, 126, 188, 216, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 7, 7, 27, 61, 126, 126, 61, 27, 7, 7, 3, 1, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_cs(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 128, 192, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 192, 224, 240, 184, 28, 14, 6, 6, 14, 28, 184, 240, 224, 192, 0, 0, 0, 0, 14, 7, 3, 1, 0, 0, 1, 3, 7, 14, 4, 0, 0, 0, 1, 1, 1, 31, 31, 48, 48, 48, 48, 31, 31, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_ca(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 128, 192, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 7, 3, 1, 0, 0, 1, 3, 7, 14, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 28, 126, 220, 152, 0, 0, 152, 252, 254, 28, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 127, 127, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_cg(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 128, 192, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 7, 3, 1, 0, 0, 1, 3, 7, 14, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 192, 224, 224, 216, 188, 126, 126, 188, 216, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 7, 7, 27, 61, 126, 126, 61, 27, 7, 7, 3, 1, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_sa(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 192, 224, 240, 184, 28, 14, 6, 6, 14, 28, 184, 240, 224, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 31, 31, 48, 48, 48, 48, 31, 31, 1, 1, 1, 0, 0, 0, 24, 28, 126, 220, 152, 0, 0, 152, 252, 254, 28, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 127, 127, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_sg(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 192, 224, 240, 184, 28, 14, 6, 6, 14, 28, 184, 240, 224, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 31, 31, 48, 48, 48, 48, 31, 31, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 192, 224, 224, 216, 188, 126, 126, 188, 216, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 7, 7, 27, 61, 126, 126, 61, 27, 7, 7, 3, 1, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_ag(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 28, 126, 220, 152, 0, 0, 152, 252, 254, 28, 24, 0, 0, 0, 128, 192, 224, 224, 216, 188, 126, 126, 188, 216, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 3, 127, 127, 3, 1, 0, 0, 0, 0, 0, 0, 1, 3, 7, 7, 27, 61, 126, 126, 61, 27, 7, 7, 3, 1, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_csa(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 128, 192, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 192, 224, 240, 184, 28, 14, 6, 6, 14, 28, 184, 240, 224, 192, 0, 0, 0, 0, 14, 7, 3, 1, 0, 0, 1, 3, 7, 14, 4, 0, 0, 0, 1, 1, 1, 31, 31, 48, 48, 48, 48, 31, 31, 1, 1, 1, 0, 0, 0, 24, 28, 126, 220, 152, 0, 0, 152, 252, 254, 28, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 127, 127, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_csg(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 128, 192, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 192, 224, 240, 184, 28, 14, 6, 6, 14, 28, 184, 240, 224, 192, 0, 0, 0, 0, 14, 7, 3, 1, 0, 0, 1, 3, 7, 14, 4, 0, 0, 0, 1, 1, 1, 31, 31, 48, 48, 48, 48, 31, 31, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 192, 224, 224, 216, 188, 126, 126, 188, 216, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 7, 7, 27, 61, 126, 126, 61, 27, 7, 7, 3, 1, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_cag(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 128, 192, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 7, 3, 1, 0, 0, 1, 3, 7, 14, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 28, 126, 220, 152, 0, 0, 152, 252, 254, 28, 24, 0, 0, 0, 128, 192, 224, 224, 216, 188, 126, 126, 188, 216, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 3, 127, 127, 3, 1, 0, 0, 0, 0, 0, 0, 1, 3, 7, 7, 27, 61, 126, 126, 61, 27, 7, 7, 3, 1, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_sag(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 192, 224, 240, 184, 28, 14, 6, 6, 14, 28, 184, 240, 224, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 31, 31, 48, 48, 48, 48, 31, 31, 1, 1, 1, 0, 0, 0, 24, 28, 126, 220, 152, 0, 0, 152, 252, 254, 28, 24, 0, 0, 0, 128, 192, 224, 224, 216, 188, 126, 126, 188, 216, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 3, 127, 127, 3, 1, 0, 0, 0, 0, 0, 0, 1, 3, 7, 7, 27, 61, 126, 126, 61, 27, 7, 7, 3, 1, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-static void render_mod_csag(void) {
-    static const char PROGMEM raw_logo[] = {
-        0, 0, 0, 0, 0, 128, 192, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 192, 224, 240, 184, 28, 14, 6, 6, 14, 28, 184, 240, 224, 192, 0, 0, 0, 0, 14, 7, 3, 1, 0, 0, 1, 3, 7, 14, 4, 0, 0, 0, 1, 1, 1, 31, 31, 48, 48, 48, 48, 31, 31, 1, 1, 1, 0, 0, 0, 24, 28, 126, 220, 152, 0, 0, 152, 252, 254, 28, 24, 0, 0, 0, 128, 192, 224, 224, 216, 188, 126, 126, 188, 216, 224, 224, 192, 128, 0, 0, 0, 0, 0, 0, 0, 3, 127, 127, 3, 1, 0, 0, 0, 0, 0, 0, 1, 3, 7, 7, 27, 61, 126, 126, 61, 27, 7, 7, 3, 1, 0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-void render_mod_state(void) {
-    uint8_t mods        = get_mods();
-    bool    is_mod_ctrl = mods & MOD_MASK_CTRL;
-    bool    is_mod_shft = mods & MOD_MASK_SHIFT;
-    bool    is_mod_alt  = mods & MOD_MASK_ALT;
-    bool    is_mod_gui  = mods & MOD_MASK_GUI;
-
-    if (is_mod_ctrl && !is_mod_shft && !is_mod_alt && !is_mod_gui) {
-        render_mod_ctrl();
-    } else if (!is_mod_ctrl && is_mod_shft && !is_mod_alt && !is_mod_gui) {
-        render_mod_shft();
-    } else if (!is_mod_ctrl && !is_mod_shft && is_mod_alt && !is_mod_gui) {
-        render_mod_alt();
-    } else if (!is_mod_ctrl && !is_mod_shft && !is_mod_alt && is_mod_gui) {
-        render_mod_gui();
-    } else if (is_mod_ctrl && is_mod_shft && !is_mod_alt && !is_mod_gui) {
-        render_mod_cs();
-    } else if (is_mod_ctrl && !is_mod_shft && is_mod_alt && !is_mod_gui) {
-        render_mod_ca();
-    } else if (is_mod_ctrl && !is_mod_shft && !is_mod_alt && is_mod_gui) {
-        render_mod_cg();
-    } else if (!is_mod_ctrl && is_mod_shft && is_mod_alt && !is_mod_gui) {
-        render_mod_sa();
-    } else if (!is_mod_ctrl && is_mod_shft && !is_mod_alt && is_mod_gui) {
-        render_mod_sg();
-    } else if (!is_mod_ctrl && !is_mod_shft && is_mod_alt && is_mod_gui) {
-        render_mod_ag();
-    } else if (is_mod_ctrl && is_mod_shft && is_mod_alt && !is_mod_gui) {
-        render_mod_csa();
-    } else if (is_mod_ctrl && is_mod_shft && !is_mod_alt && is_mod_gui) {
-        render_mod_csg();
-    } else if (is_mod_ctrl && !is_mod_shft && is_mod_alt && is_mod_gui) {
-        render_mod_cag();
-    } else if (!is_mod_ctrl && is_mod_shft && is_mod_alt && is_mod_gui) {
-        render_mod_sag();
-    } else if (is_mod_ctrl && is_mod_shft && is_mod_alt && is_mod_gui) {
-        render_mod_csag();
-    } else {
-        render_blank();
-    }
+static void render_cmd(bool pressed) {
+    static const char PROGMEM cmd_icon[]         = {0x66, 0xa5, 0xff, 0x24, 0x24, 0xff, 0xa5, 0x66};
+    static const char PROGMEM cmd_icon_pressed[] = {0x99, 0x5a, 0x00, 0xdb, 0xdb, 0x00, 0x5a, 0x99};
+    oled_write_raw_P(pressed ? cmd_icon_pressed : cmd_icon, 8);
 }
 
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         // QMK Logo and version information
         // clang-format off
-        // static const char PROGMEM qmk_logo[] = {
-        //     0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        //     0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        //     0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-        // // clang-format on
-        //
-        // oled_write_P(qmk_logo, false);
-        // oled_write_P(PSTR("\nKyria rev3.1\n\n"), false);
-        //
-        // // Host Keyboard Layer Status
-        // oled_write_P(PSTR("Layer: "), false);
-        // switch (get_highest_layer(layer_state | default_layer_state)) {
-        //     case _BASE:
-        //         oled_write_P(PSTR("Base\n"), false);
-        //         break;
-        //     case _BASE_NO_HRM:
-        //         oled_write_P(PSTR("Base W/o HRM\n"), false);
-        //         break;
-        //     case _LOWER:
-        //         oled_write_P(PSTR("Lower\n"), false);
-        //         break;
-        //     case _RAISE:
-        //         oled_write_P(PSTR("Raise\n"), false);
-        //         break;
-        //     case _NAV_3D:
-        //         oled_write_P(PSTR("3D Nav\n"), false);
-        //         break;
-        //     default:
-        //         oled_write_P(PSTR("Undefined\n"), false);
-        // }
-        //
+        static const char PROGMEM qmk_logo[] = {
+            0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+            0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+            0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
+        // clang-format on
 
-        render_apple_logo();
-        oled_set_cursor(0, 4);
-        // oled_set_cursor(0, 6);
-        // uint8_t modifiers = get_mods();
-        //
-        // render_shift(modifiers & MOD_MASK_SHIFT);
-        // oled_advance_char();
-        // oled_advance_char();
-        // render_ctrl(modifiers & MOD_MASK_CTRL);
-        // oled_advance_char();
-        // oled_advance_char();
-        // render_option(modifiers & MOD_MASK_ALT);
-        // oled_advance_char();
-        // oled_advance_char();
-        // render_cmd(modifiers & MOD_MASK_GUI);
-        render_mod_state();
+        oled_write_P(qmk_logo, false);
+        // oled_write_P(PSTR("\nKyria rev3.1\n\n"), false);
+
+        // Host Keyboard Layer Status
+        oled_write_P(PSTR("Layer: "), false);
+        switch (get_highest_layer(layer_state | default_layer_state)) {
+            case _BASE:
+                oled_write_P(PSTR("Base\n"), false);
+                break;
+            case _BASE_NO_HRM:
+                oled_write_P(PSTR("Base W/o HRM\n"), false);
+                break;
+            case _LOWER:
+                oled_write_P(PSTR("Lower\n"), false);
+                break;
+            case _RAISE:
+                oled_write_P(PSTR("Raise\n"), false);
+                break;
+            case _NAV_3D:
+                oled_write_P(PSTR("3D Nav\n"), false);
+                break;
+            default:
+                oled_write_P(PSTR("Undefined\n"), false);
+        }
+
+        oled_set_cursor_raw(0, 6);
+        uint8_t modifiers = get_mods();
+
+        render_shift(modifiers & MOD_MASK_SHIFT);
+        oled_advance_char();
+        oled_advance_char();
+        render_ctrl(modifiers & MOD_MASK_CTRL);
+        oled_advance_char();
+        oled_advance_char();
+        render_option(modifiers & MOD_MASK_ALT);
+        oled_advance_char();
+        oled_advance_char();
+        render_cmd(modifiers & MOD_MASK_GUI);
     } else {
         // clang-format off
         static const char PROGMEM kyria_logo[] = {
