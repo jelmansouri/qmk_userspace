7435cee535201560a8ab29397e3b344b531e62b1
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-08-25 14:56:05 -0300

    Remove caps in favor of tab on nav 3d layer

--- keyboards/splitkb/kyria/keymaps/jelmansouri/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jelmansouri/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NAV_3D] = LAYOUT_split_3x6_5(
-        KC_NO, KC_CAPS,    KC_Q,    KC_W,    KC_E,    KC_R,                                     RGB_TOG, RGB_MOD, RGB_HUI, RGB_VAI, RGB_SPI, RGB_M_K,
+        KC_NO,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                                     RGB_TOG, RGB_MOD, RGB_HUI, RGB_VAI, RGB_SPI, RGB_M_K,
         KC_NO, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                                     RGB_TOG, RGB_RMOD, RGB_HUD, RGB_VAD, RGB_SPD, RGB_M_X,
         KC_NO, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,   KC_TRNS,  KC_SPC,   KC_NO,   KC_NO, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_G,
                                  KC_LALT, KC_LGUI,   KC_NO,   KC_TRNS,    BASE,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
