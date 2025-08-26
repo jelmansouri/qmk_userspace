2719573ebc5e5f49878f4dc613c07120823e63e1
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-08-26 05:11:18 -0300

    Fix issues

--- keyboards/splitkb/halcyon/kyria/keymaps/jelmansouri_hlc/display.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/jelmansouri_hlc/display.c
@@ -2,37 +2,37 @@
 #include <stdint.h>
 #include "hlc_tft_display/hlc_tft_display.h"
 
-#include "raphics/modifiers/caps_word_enabled.qgf.h"
-#include "raphics/modifiers/caps_word_shift_pressed.qgf.h"
-#include "raphics/modifiers/command_pressed.qgf.h"
-#include "raphics/modifiers/command_unpressed.qgf.h"
-#include "raphics/modifiers/control_pressed.qgf.h"
-#include "raphics/modifiers/control_unpressed.qgf.h"
-#include "raphics/modifiers/option_pressed.qgf.h"
-#include "raphics/modifiers/option_unpressed.qgf.h"
-#include "raphics/modifiers/shift_pressed.qgf.h"
-#include "raphics/modifiers/shift_unpressed.qgf.h"
+#include "graphics/modifiers/caps_word_enabled.qgf.h"
+#include "graphics/modifiers/caps_word_shift_pressed.qgf.h"
+#include "graphics/modifiers/command_pressed.qgf.h"
+#include "graphics/modifiers/command_unpressed.qgf.h"
+#include "graphics/modifiers/control_pressed.qgf.h"
+#include "graphics/modifiers/control_unpressed.qgf.h"
+#include "graphics/modifiers/option_pressed.qgf.h"
+#include "graphics/modifiers/option_unpressed.qgf.h"
+#include "graphics/modifiers/shift_pressed.qgf.h"
+#include "graphics/modifiers/shift_unpressed.qgf.h"
 
-#include "raphics/layers/base_layer_icon_colored.qgf.h"
-#include "raphics/layers/base_layer_text_colored.qgf.h"
-#include "raphics/layers/lower_layer_icon_colored.qgf.h"
-#include "raphics/layers/lower_layer_text_colored.qgf.h"
-#include "raphics/layers/nav_3d_layer_icon_colored.qgf.h"
-#include "raphics/layers/nav_3d_layer_text_colored.qgf.h"
-#include "raphics/layers/raise_layer_icon_colored.qgf.h"
-#include "raphics/layers/raise_layer_text_colored.qgf.h"
+#include "graphics/layers/base_layer_icon_colored.qgf.h"
+#include "graphics/layers/base_layer_text_colored.qgf.h"
+#include "graphics/layers/lower_layer_icon_colored.qgf.h"
+#include "graphics/layers/lower_layer_text_colored.qgf.h"
+#include "graphics/layers/nav_3d_layer_icon_colored.qgf.h"
+#include "graphics/layers/nav_3d_layer_text_colored.qgf.h"
+#include "graphics/layers/raise_layer_icon_colored.qgf.h"
+#include "graphics/layers/raise_layer_text_colored.qgf.h"
 
 #include "keymap.h"
 
 typedef struct layer_gfx_definition {
     painter_image_handle_t icon;
     painter_image_handle_t text;
-} layer_gfx_def;
+} layer_gfx_definition;
 
 typedef struct modifier_gfx_definition {
     painter_image_handle_t pressed;
     painter_image_handle_t unpressed;
-} modifier_gfx_def;
+} modifier_gfx_definition;
 
 // Setup painter devices
 extern painter_device_t lcd;
@@ -40,11 +40,11 @@ extern painter_device_t lcd_surface;
 
 static layer_gfx_definition layer_mapping[_COUNT] = {0};
 
-static painter_image_handle_t shift;
-static painter_image_handle_t control;
-static painter_image_handle_t option;
-static painter_image_handle_t command;
-static painter_image_handle_t caps_word;
+static modifier_gfx_definition shift;
+static modifier_gfx_definition control;
+static modifier_gfx_definition option;
+static modifier_gfx_definition command;
+static modifier_gfx_definition caps_word;
 
 // This function is ran on bootup of the keyboard
 bool module_post_init_user(void) {
@@ -60,8 +60,8 @@ bool module_post_init_user(void) {
     layer_mapping[_RAISE].text             = qp_load_image_mem(gfx_raise_layer_text_colored);
     layer_mapping[_NAV_3D].icon            = qp_load_image_mem(gfx_nav3d_layer_icon_colored);
     layer_mapping[_NAV_3D].text            = qp_load_image_mem(gfx_nav3d_layer_text_colored);
-    layer_mapping[_COUNT].icon             = qp_load_image_mem(gfx_undefined_layer_icon);
-    layer_mapping[_COUNT].text             = qp_load_image_mem(gfx_undefined_layer_text);
+    layer_mapping[_UNDEFINED].icon         = qp_load_image_mem(gfx_undefined_layer_icon);
+    layer_mapping[_UNDEFINED].text         = qp_load_image_mem(gfx_undefined_layer_text);
 
     shift.pressed       = qp_load_image_mem(gfx_shift_pressed);
     shift.unpressed     = qp_load_image_mem(gfx_shift_unpressed);
@@ -71,8 +71,8 @@ bool module_post_init_user(void) {
     option.unpressed    = qp_load_image_mem(gfx_option_unpressed);
     command.pressed     = qp_load_image_mem(gfx_command_pressed);
     command.unpressed   = qp_load_image_mem(gfx_command_unpressed);
-    caps_word.pressed   = qp_load_image_mem(gfx_caps_word_enabled);
-    caps_word.unpressed = qp_load_image_mem(gfx_caps_word_shift_pressed);
+    caps_word.pressed   = qp_load_image_mem(gfx_caps_word_shift_pressed);
+    caps_word.unpressed = qp_load_image_mem(gfx_caps_word_enabled);
 
     return false;
 }
@@ -92,8 +92,8 @@ bool display_module_housekeeping_task_user(bool second_display) {
         if (!layer_initialized || current_layer != last_layer) {
             in_or_out_of_no_hrm         = (last_layer == _BASE_NO_HRM) || (current_layer == _BASE_NO_HRM);
             layer_gfx_definition* layer = layer_mapping[current_layer > _UNDEFINED ? _UNDEFINED : current_layer];
-            qp_drawimage(lcd_surface, (LCD_WIDTH - layer->icon->width) >> 1, ((LCD_HEIGHT >> 1) - layer->icon->height) >> 1, layer_mapping[layer_idx]);
-            qp_drawimage(lcd_surface, (LCD_WIDTH - layer->text->width) >> 1, (LCD_HEIGHT - layer->icon->height) >> 1, layer_mapping[layer_idx]);
+            qp_drawimage(lcd_surface, (LCD_WIDTH - layer->icon->width) >> 1, ((LCD_HEIGHT >> 1) - layer->icon->height) >> 1, layer->icon);
+            qp_drawimage(lcd_surface, (LCD_WIDTH - layer->text->width) >> 1, (LCD_HEIGHT - layer->text->height) >> 1, layer->text);
             layer_initialized = true;
             last_layer        = current_layer;
         }
@@ -110,7 +110,7 @@ bool display_module_housekeeping_task_user(bool second_display) {
                 const uint16_t         x      = (LCD_WIDTH - width * 3) >> 1;
                 const uint16_t         y      = LCD_HEIGHT - (((LCD_HEIGHT >> 1) - height) >> 1);
                 if (current_layer == _BASE_NO_HRM) {
-                    qp_rect(lcd, x, y, width - 1, height - 1, HSV_BLACK, true);
+                    qp_rect(lcd_surface, x, y, width - 1, height - 1, HSV_BLACK, true);
                 } else {
                     qp_drawimage(lcd_surface, x, y, status);
                 }
@@ -122,7 +122,7 @@ bool display_module_housekeeping_task_user(bool second_display) {
                 const uint16_t         x      = (LCD_WIDTH - width) >> 1;
                 const uint16_t         y      = LCD_HEIGHT - (((LCD_HEIGHT >> 1) - height) >> 1);
                 if (current_layer == _BASE_NO_HRM) {
-                    qp_rect(lcd, x, y, width - 1, height - 1, HSV_BLACK, true);
+                    qp_rect(lcd_surface, x, y, width - 1, height - 1, HSV_BLACK, true);
                 } else {
                     qp_drawimage(lcd_surface, x, y, status);
                 }
@@ -134,14 +134,14 @@ bool display_module_housekeeping_task_user(bool second_display) {
                 const uint16_t         x      = (LCD_WIDTH + width) >> 1;
                 const uint16_t         y      = LCD_HEIGHT - (((LCD_HEIGHT >> 1) - height) >> 1);
                 if (current_layer == _BASE_NO_HRM) {
-                    qp_rect(lcd, x, y, width - 1, height - 1, HSV_BLACK, true);
+                    qp_rect(lcd_surface, x, y, width - 1, height - 1, HSV_BLACK, true);
                 } else {
                     qp_drawimage(lcd_surface, x, y, status);
                 }
             }
             if (changed_modifiers & MOD_MASK_SHIFT || caps_word_changed) {
                 modifier_gfx_definition* modifier = current_caps_word_on ? &caps_word : &shift;
-                painter_image_handle_t   status   = current_modifiers & MOD_MASK_GUI ? modifier.pressed : modifier.unpressed;
+                painter_image_handle_t   status   = current_modifiers & MOD_MASK_SHIFT ? modifier.pressed : modifier.unpressed;
                 const uint16_t           width    = status->width;
                 const uint16_t           height   = status->height;
                 const uint16_t           x        = (LCD_WIDTH - width) >> 1;
