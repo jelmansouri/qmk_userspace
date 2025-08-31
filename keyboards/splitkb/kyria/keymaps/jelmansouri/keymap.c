34edd9d116d4aa8cf1f306cc05ac17a5123ab335
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-08-31 01:39:58 -0300

    debug hold count

--- keyboards/splitkb/kyria/keymaps/jelmansouri/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jelmansouri/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool set_scrolling = false;
 
 // Track hold state of left hand mod-tap keys
-static uint8_t right_mod_hold_count = 0;
+uint8_t right_mod_hold_count = 0;
 
 // Handle new Mod Tap shifted keycodes as they are not supported using the MT macro
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
--- keyboards/splitkb/kyria/keymaps/jelmansouri/oled.c
+++ keyboards/splitkb/kyria/keymaps/jelmansouri/oled.c
@@ -217,6 +217,8 @@ void oled_write_icon(const char* PROGMEM icon, uint8_t col, uint8_t line) {
     oled_write_raw_P(icon + HALF_ICON_SIZE, HALF_ICON_SIZE);
 }
 
+extern uint8_t right_mod_hold_count;
+
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         // Host Keyboard Layer Status
@@ -249,6 +251,10 @@ bool oled_task_user(void) {
             final_shift_icon =
                 (get_weak_mods() & MOD_MASK_SHIFT) ? caps_word_enabled_shift_pressed_icon : caps_word_enabled_icon;
         }
+
+        oled_set_cursor(0, 5);
+
+        oled_write_char('0' + left_mod_hold_count, false);
 
         uint8_t start_x = oled_rotation_width >> 2;
         if (is_hrm_disabled) {
