fe89a1632557b577477b323c6b6fc46a22cc6abb
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2026-04-28 23:25:53 +0200

    Cleanup settings

--- keyboards/cyboard/imprint/imprint_letters_only_2key_bottom_row/keymaps/jelmansouri/rules.mk
+++ keyboards/cyboard/imprint/imprint_letters_only_2key_bottom_row/keymaps/jelmansouri/rules.mk
@@ -1,11 +1,2 @@
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-LTO_ENABLE = no
-QMK_SETTINGS = yes
-TAP_DANCE_ENABLE = yes
-COMBO_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
-VIALRGB_ENABLE = yes
-
 # extra sources
 SRC += rgb_matrix.c
